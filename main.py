3b9ed84331f735358497e4826e1b0ab414211d5b
enchance <enchance@gmail.com>
2024-10-10 11:35:18 +0800

    integrate api key

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-import httpx
+import httpx, os
+from dotenv import load_dotenv
 
-API_KEY = ''
+
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+
 
 def main():
     request = httpx.get('https://jsonplaceholder.typicode.com/users')
     print(request.json())
+    # print(API_KEY)
 
 
 if __name__ == '__main__':
